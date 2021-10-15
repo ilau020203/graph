2784fe8efb3348b59f18b0011f1c487b5d656acc
ilau020203 <antip19052016@gmail.com>
2021-10-15 15:34:52 +0300

    fix decimal

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,20 +1,259 @@
+
+// import { BigInt,log ,Bytes} from "@graphprotocol/graph-ts"
+// import {loadToken} from "./utils/TokenController"
+
+// import {
+//   Treasury,
+//   ChangeActivated,
+//   ChangeQueued,
+//   CreateDebt,
+//   Deposit,
+//   OwnershipPulled,
+//   OwnershipPushed,
+//   RepayDebt,
+//   ReservesAudited,
+//   ReservesManaged,
+//   ReservesUpdated,
+//   RewardsMinted,
+//   Withdrawal,
+//   DepositCall,
+//   WithdrawCall,
+//   ManageCall
+// } from "../generated/Treasury/Treasury"
+// import { ExampleEntity,DepositEntity,DepositFunctionEntity ,DebtEntity, ReservesManagedEntity,RewardsMintedEntity} from "../generated/schema"
+// import { toDecimal } from "./utils/Decimals"
+// import {totalReservesAdded}from "./utils/YearsTotalReserves"
+// import {DepositAdded}from "./utils/YearsDeposit"
+// import {ManageAdded}from "./utils/YearsManage"
+// import {rewardsMintedAdded}from "./utils/YearsRewardsMinted"
+
+
+// /***Действия:**
+// - Deposit — депонирует резервы / позволяет утвержденным адресам депонировать актив для OHM. Параметры для агрегирования: sender, token, amount, value.
+// - Withdrawal — выводит резервы / позволяет утвержденным адресам сжигать OHM для резервов. Параметры для агрегирования: sender, token, amount, value.
+// - Debt creation — позволяет утвержденным адресам заимствовать резервы. Параметры для агрегирования: debtor, token, amount, value.
+// - Debt repay — позволяет утвержденным адресам погашать заимствованные резервы либо резервами, либо OHM. Параметры для агрегирования: debtor, token, amount, value.
+// - Manage reserves — позволяет утвержденным адресам выводить активы. Параметры для агрегирования: manager (роль для ликвидного или резервного менеджера - скорее всего, кто-то из команды или внешний контракт), token, amount.
+// - Mint rewards — отправляет вознаграждения за epoch в staking контракт. Параметры для агрегирования:  caller (sender), recipient, amount.
+// **Действия по оценке резервов:**
+// - Audit reserves — оценка общих резервов (токены резервов + токены ликвидности), проводимая внешним менеджером.  Параметры для агрегирования:  total reserves.
+// - Update reserves — обновить общую стоимость резервов. Параметры для агрегирования: total reserves.
+// **Действия по управлению:**
+// - Change queued — любые изменения, касающиеся управления очередью.
+// - Change activated — произошли какие-либо изменения, касающиеся управления (например, кто может расходовать резервы).
+// */
+
+
+
+
+// export function handleChangeActivated(event: ChangeActivated): void {
+//   // Entities can be loaded from the store using a string ID; this ID
+//   // needs to be unique across all entities of the same type
+//   let entity = ExampleEntity.load(event.transaction.from.toHex())
+
+//   // Entities only exist after they have been saved to the store;
+//   // `null` checks allow to create entities on demand
+//   if (!entity) {
+//     entity = new ExampleEntity(event.transaction.from.toHex())
+
+//     // Entity fields can be set using simple assignments
+//     entity.count = BigInt.fromI32(0)
+//   }
+
+//   // BigInt and BigDecimal math are supported
+//   entity.count = entity.count + BigInt.fromI32(1)
+
+//   // Entity fields can be set based on event parameters
+//   entity.managing = event.params.managing
+//   entity.activated = event.params.activated
+
+//   // Entities can be written to the store with `.save()`
+//   entity.save()
+
+//   // Note: If a handler doesn't require existing field values, it is faster
+//   // _not_ to load the entity from the store. Instead, create it fresh with
+//   // `new Entity(...)`, set the fields that should be updated and save the
+//   // entity back to the store. Fields that were not set or unset remain
+//   // unchanged, allowing for partial updates to be applied.
+
+//   // It is also possible to access smart contracts from mappings. For
+//   // example, the contract that has emitted the event can be connected to
+//   // with:
+//   //
+//   // let contract = Contract.bind(event.address)
+//   //
+//   // The following functions can then be called on this contract to access
+//   // state variables and other data:
+//   //
+//   // - contract.LiquidityDepositorQueue(...)
+//   // - contract.LiquidityManagerQueue(...)
+//   // - contract.LiquidityTokenQueue(...)
+//   // - contract.OHM(...)
+//   // - contract.ReserveManagerQueue(...)
+//   // - contract.blocksNeededForQueue(...)
+//   // - contract.bondCalculator(...)
+//   // - contract.debtorBalance(...)
+//   // - contract.debtorQueue(...)
+//   // - contract.debtors(...)
+//   // - contract.deposit(...)
+//   // - contract.excessReserves(...)
+//   // - contract.isDebtor(...)
+//   // - contract.isLiquidityDepositor(...)
+//   // - contract.isLiquidityManager(...)
+//   // - contract.isLiquidityToken(...)
+//   // - contract.isReserveDepositor(...)
+//   // - contract.isReserveManager(...)
+//   // - contract.isReserveSpender(...)
+//   // - contract.isReserveToken(...)
+//   // - contract.isRewardManager(...)
+//   // - contract.liquidityDepositors(...)
+//   // - contract.liquidityManagers(...)
+//   // - contract.liquidityTokens(...)
+//   // - contract.manager(...)
+//   // - contract.queue(...)
+//   // - contract.reserveDepositorQueue(...)
+//   // - contract.reserveDepositors(...)
+//   // - contract.reserveManagers(...)
+//   // - contract.reserveSpenderQueue(...)
+//   // - contract.reserveSpenders(...)
+//   // - contract.reserveTokenQueue(...)
+//   // - contract.reserveTokens(...)
+//   // - contract.rewardManagerQueue(...)
+//   // - contract.rewardManagers(...)
+//   // - contract.sOHM(...)
+//   // - contract.sOHMQueue(...)
+//   // - contract.toggle(...)
+//   // - contract.totalDebt(...)
+//   // - contract.totalReserves(...)
+//   // - contract.valueOf(...)
+// }
+
+// export function handleChangeQueued(event: ChangeQueued): void {}
+
+// export function handleCreateDebt(event: CreateDebt): void {
+//   let debt = new DebtEntity(event.transaction.from.toHex())
+//   debt.value= toDecimal(event.params.value, 18); 
+//   debt.amount = toDecimal(event.params.amount, 18);
+//   debt.debtor =event.params.debtor;
+//   let token =loadToken(event.params.token,event.block.timestamp);
+//   debt.token =token.id;
+//   debt.creation=true;
+//   debt.save();
+// }
+
+// export function handleDeposit(event: Deposit): void {
+//   let deposit = new DepositEntity(event.transaction.hash.toHex());
+//   deposit.timestamp= event.block.timestamp;
+//   deposit.address = event.params.token;
+//   deposit.value =toDecimal(event.params.value, 18); 
+//   deposit.amount = toDecimal(event.params.amount, 18);
+//   deposit.save();
+// }
+
+
+// export function handleDepositFunction(call: DepositCall): void {
+//   let id = call.transaction.hash.toHex();
+//   let event = DepositEntity.load(id);
+//   let token =loadToken(call.inputs._token,call.block.timestamp);
+
+//   DepositAdded(call.from,token.id,toDecimal(call.inputs._profit, 18),event?event.value:toDecimal(BigInt.zero(),0), toDecimal(call.inputs._amount, 9),call.block.timestamp,true);
+ 
+// }
+
+// export function handleOwnershipPulled(event: OwnershipPulled): void {}
+
+// export function handleOwnershipPushed(event: OwnershipPushed): void {}
+
+// export function handleRepayDebt(event: RepayDebt): void {
+//   let debt = new DebtEntity(event.transaction.from.toHex())
+//   debt.value= toDecimal(event.params.value, 18); 
+//   debt.amount = toDecimal(event.params.amount, 18);
+//   debt.debtor = event.params.debtor;
+//   let token =loadToken(event.params.token,event.block.timestamp);
+//   debt.token =token.id;
+//   debt.creation=false;
+//   debt.save();
+// }
+
+// export function handleReservesAudited(event: ReservesAudited): void {
+//   totalReservesAdded(toDecimal(event.params.totalReserves, 9),true,event.block.timestamp);
+// }
+
+// export function handleReservesManaged(event: ReservesManaged): void {
+//   let reservesManaged=new ReservesManagedEntity(event.transaction.hash.toHex());
+//   reservesManaged.token= event.params.token;
+//   reservesManaged.amount=toDecimal(event.params.amount, 18);
+//   reservesManaged.timestamp=event.block.timestamp;
+//   reservesManaged.save();
+// }
+
+
+// export function handleReservesUpdated(event: ReservesUpdated): void {
+//   totalReservesAdded(toDecimal(event.params.totalReserves, 9),false,event.block.timestamp);
+// }
+
+// export function handleRewardsMinted(event: RewardsMinted): void {
+//   //0xe6295201cd1ff13ced5f063a5421c39a1d236f1c
+//   if(event.params.recipient.toHexString()=="0xe6295201cd1ff13ced5f063a5421c39a1d236f1c"){
+//     return;
+//   }
+//   rewardsMintedAdded(toDecimal(event.params.amount, 9),event.params.caller,event.params.recipient,event.block.timestamp)
+// }
+
+// export function handleWithdrawal(event: Withdrawal): void {
+//   let withdrawal = new DepositEntity(event.transaction.hash.toHex());
+//   withdrawal.timestamp = event.block.timestamp;
+//   withdrawal.address = event.params.token;
+//   withdrawal.value =toDecimal(event.params.value, 18); 
+//   withdrawal.amount = toDecimal(event.params.amount, 18);
+//   withdrawal.save();
+// }
+
+
+// export function handleWithdrawFunction(call: WithdrawCall): void {
+//   let id = call.transaction.hash.toHex();
+//   let event = DepositEntity.load(id);
+//   let deposit = new DepositFunctionEntity(id);
+//   deposit.value=event?event.value:toDecimal(BigInt.zero(),0);
+//   deposit.amount= toDecimal(call.inputs._amount, 18);
+//   deposit.sender =call.from;
+//   deposit.timestamp= call.block.timestamp;
+//   deposit.isDeposit=false;
+//   deposit.save();
+// }
+// export function handleManageFunction(call:ManageCall):void{
+//   let token =loadToken(call.inputs._token,call.block.timestamp);
+//   ManageAdded(call.from,token.id,toDecimal(call.inputs._amount,9),call.block.timestamp);
+// }
 import { BigInt,log ,Bytes} from "@graphprotocol/graph-ts"
 import {loadToken} from "./utils/TokenController"
 
 import {
+  Treasury,
+  ChangeActivated,
+  ChangeQueued,
+  CreateDebt,
   Deposit,
+  OwnershipPulled,
+  OwnershipPushed,
+  RepayDebt,
   ReservesAudited,
+  ReservesManaged,
   ReservesUpdated,
   RewardsMinted,
+  Withdrawal,
   DepositCall,
+  WithdrawCall,
   ManageCall
 } from "../generated/Treasury/Treasury"
-import { DepositEntity } from "../generated/schema"
+import { DepositEntity} from "../generated/schema"
 import { toDecimal } from "./utils/Decimals"
 import {totalReservesAdded}from "./utils/YearsTotalReserves"
 import {DepositAdded}from "./utils/YearsDeposit"
 import {ManageAdded}from "./utils/YearsManage"
 import {rewardsMintedAdded}from "./utils/YearsRewardsMinted"
+
+
 
 /**
  * save Deposit event
@@ -24,7 +263,7 @@ export function handleDeposit(event: Deposit): void {
   let deposit = new DepositEntity(event.transaction.hash.toHex());
   deposit.timestamp= event.block.timestamp;
   deposit.address = event.params.token;
-  deposit.value =toDecimal(event.params.value, 18); 
+  deposit.value = toDecimal(event.params.value, 9); 
   deposit.amount = toDecimal(event.params.amount, 18);
   deposit.save();
 }
@@ -71,5 +310,22 @@ export function handleRewardsMinted(event: RewardsMinted): void {
  */
 export function handleManageFunction(call:ManageCall):void{
   let token =loadToken(call.inputs._token,call.block.timestamp);
-  ManageAdded(call.from,token.id,toDecimal(call.inputs._amount,9),call.block.timestamp);
+  ManageAdded(call.from,token.id,toDecimal(call.inputs._amount,18),call.block.timestamp);
 }
+
+
+
+
+
+
+
+
+export function handleChangeActivated(event: ChangeActivated): void {}
+export function handleChangeQueued(event: ChangeQueued): void {}
+export function handleCreateDebt(event: CreateDebt): void {}
+export function handleOwnershipPulled(event: OwnershipPulled): void {}
+export function handleOwnershipPushed(event: OwnershipPushed): void {}
+export function handleRepayDebt(event: RepayDebt): void {}
+export function handleReservesManaged(event: ReservesManaged): void {}
+export function handleWithdrawal(event: Withdrawal): void {}
+export function handleWithdrawFunction(call: WithdrawCall): void {}
